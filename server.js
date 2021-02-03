0f442be33f9b7b88719f9ada503b77b0743942a7
Genis Bardales <genis96@yahoo.com>
2021-02-02 22:22:15 -0500

    started server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const db = 'db/db.json';
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
