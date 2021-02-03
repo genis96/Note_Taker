6b0455736834dbcad0797056face789a7ebc2f23
Genis Bardales <genis96@yahoo.com>
2021-02-02 22:28:11 -0500

    add notes

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ const db = 'db/db.json';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// PARSING, STATIC AND ROUTES
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
+
+// let dataNotes = [];
+
+// GET
+
+
+// POST
+
+
+// DELETE
+
+
+// HTML GET ROUTES
+
